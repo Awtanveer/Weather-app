6710187594d93266fd44c6c5c994f86108e91fbd
Awtanveer <ec19055@qmil.ac.uk>
2021-03-10 21:52:25 +0000

    added winddpeed/direction and parial completion for future weather

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -18,12 +18,16 @@ export default class Iphone extends Component {
 		this.state.temp = "";
 		// button display state
 		this.setState({ display: true });
+		this.setState({dailytarr:["null","null","null","null","null","null","null"]});
+		this.setState({dailywsarr:["null","null","null","null","null","null","null"]});
+		this.setState({dailywdarr:["null","null","null","null","null","null","null"]});
 	}
 
 	// a call to fetch weather data via wunderground
 	fetchWeatherData = () => {
+		this.fetchWeatherData2();
 		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
-		var url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=cf17e23b1d108b29a4d738d2084baf5";
+		var url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=d6f78e072487f24bf48750fd7a0f66d2";
 		$.ajax({
 			url: url,
 			dataType: "jsonp",
@@ -34,6 +38,49 @@ export default class Iphone extends Component {
 		this.setState({ display: false });
 	}
 
+	//a call to fetch future weather data
+	fetchWeatherData2 = () => {
+		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
+		var url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly&appid=d6f78e072487f24bf48750fd7a0f66d2";
+		$.ajax({
+			url: url,
+			dataType: "jsonp",
+			success : this.parseResponse2,
+			error : function(req, err){ console.log('API call failed ' + err); }
+		})
+		// once the data grabbed, hide the button
+		this.setState({ display: false });
+	}
+
+	//Calculates direction from degree obtained via api
+	//needs bounds to be fixed
+	degdirec = (degree) => {
+		if(degree<22.5 && degree>337.5){
+			return "N";
+		}
+		else if(degree>=22.5 && degree<67.5){
+			return "NE"
+		}
+		else if(degree>67.5 && degree<112.5){
+			return "E"
+		}
+		else if(degree>=112.5 && degree<157.5){
+			return "SE"
+		}
+		else if(degree>157.5 && degree<202.5){
+			return "S"
+		}
+		else if(degree>=202.5 && degree<247.5){
+			return "SW"
+		}
+		else if(degree>247.5 && degree<292.5){
+			return "W"
+		}
+		else if(degree>=292.5 && degree<337.5){
+			return "NW"
+		}
+		
+	}
 	// the main render method for the iphone component
 	render() {
 		// check if temperature data is fetched, if so add the sign styling to the page
@@ -46,6 +93,9 @@ export default class Iphone extends Component {
 					<div class={ style.city }>{ this.state.locate }</div>
 					<div class={ style.conditions }>{ this.state.cond }</div>
 					<span class={ tempStyles }>{ this.state.temp }</span>
+					<div class={ style.city }>{ this.state.speed }</div>
+					<div class={ style.city }>{ this.state.degree }</div>
+					<div class={ style.city }>{this.degdirec(this.state.degree)}</div>
 				</div>
 				<div class={ style.details }></div>
 				<div class= { style_iphone.container }> 
@@ -59,12 +109,34 @@ export default class Iphone extends Component {
 		var location = parsed_json['name'];
 		var temp_c = parsed_json['main']['temp'];
 		var conditions = parsed_json['weather']['0']['description'];
+		var windspeed = parsed_json['wind']['speed'];
+		var winddeg = parsed_json['wind']['deg'];
 
 		// set states for fields so they could be rendered later on
 		this.setState({
 			locate: location,
 			temp: temp_c,
-			cond : conditions
+			cond : conditions,
+			speed : windspeed,
+			degree : winddeg
+		});      
+	}
+	parseResponse2 = (parsed_json) => {
+		var dailytemp = [];
+		var dailyspeed = [];
+		var dailydegree = [];
+		
+		for (let index = 0; index < 7; index++) {
+			dailytemp.push(parsed_json['daily'][i]['temp']['day']);
+			dailyspeed.push(parsed_json['daily'][i]['wind_speed']);
+			dailydegree.push(parsed_json['daily'][i]['wind_degree']);
+		}
+
+		// set states for fields so they could be rendered later on
+		this.setState({
+			dailytarr : dailytemp,
+			dailywsarr : dailyspeed,
+			dailywdarr : dailydegree
 		});      
 	}
 }
