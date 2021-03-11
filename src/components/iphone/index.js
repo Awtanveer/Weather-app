f56bcb39e595ab64623a7bcf6595ac2780cc4f3f
Awtanveer <ec19055@qmil.ac.uk>
2021-03-11 22:38:12 +0000

    added icons to table

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -18,13 +18,16 @@ export default class Iphone extends Component {
 		this.state.temp = "";
 		// button display state
 		this.setState({ display: true });
-		this.setState({dailytarr:["null","null","null","null","null","null","null"]});
-		this.setState({dailywsarr:["null","null","null","null","null","null","null"]});
-		this.setState({dailywdarr:["null","null","null","null","null","null","null"]});
+		this.setState({dailyTArr:["null","null","null","null","null","null","null"]});
+		this.setState({dailyWSArr:["null","null","null","null","null","null","null"]});
+		this.setState({dailyWDArr:["null","null","null","null","null","null","null"]});
+		this.setState({dailyIArr:["null","null","null","null","null","null","null"]});
+		this.setState({future: false});
 	}
 
 	// a call to fetch weather data via wunderground
 	fetchWeatherData = () => {
+		this.setState({future:true});
 		this.fetchWeatherData2();
 		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
 		var url = "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=d6f78e072487f24bf48750fd7a0f66d2";
@@ -41,7 +44,7 @@ export default class Iphone extends Component {
 	//a call to fetch future weather data
 	fetchWeatherData2 = () => {
 		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
-		var url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly&appid=d6f78e072487f24bf48750fd7a0f66d2";
+		var url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly&units=metric&appid=d6f78e072487f24bf48750fd7a0f66d2";
 		$.ajax({
 			url: url,
 			dataType: "jsonp",
@@ -81,11 +84,153 @@ export default class Iphone extends Component {
 		}
 		
 	}
+
+	degdirec2 = (dailyWDArr) => {
+		if(dailyWDArr<22.5 && dailyWDArr>337.5){
+			return "N";
+		}
+		else if(dailyWDArr>=22.5 && dailyWDArr<67.5){
+			return "NE"
+		}
+		else if(dailyWDArr>67.5 && dailyWDArr<112.5){
+			return "E"
+		}
+		else if(dailyWDArr>=112.5 && dailyWDArr<157.5){
+			return "SE"
+		}
+		else if(dailyWDArr>157.5 && dailyWDArr<202.5){
+			return "S"
+		}
+		else if(dailyWDArr>=202.5 && dailyWDArr<247.5){
+			return "SW"
+		}
+		else if(dailyWDArr>247.5 && dailyWDArr<292.5){
+			return "W"
+		}
+		else if(dailyWDArr>=292.5 && dailyWDArr<337.5){
+			return "NW"
+		}
+		
+	}
+
+	//creates table for weather in upcoming days
+	ShowFuture = () => {
+		return(
+			<div>
+				{this.state.dailyTArr[1]}
+				<table>
+					<tr>
+						<td>
+							Day
+						</td>
+						<td>
+							Temperature
+						</td>
+						<td>
+							
+						</td>
+						<td>
+							Wind Speed
+						</td>
+						<td>
+							Wind Direction
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Day 1
+						</td>
+						<td>
+							{this.state.dailyTArr[1]}
+						</td>
+						<td>
+							<img src = {this.state.dailyIArr[1]}/>
+						</td>
+						<td>
+							{this.state.dailyWSArr[1]}
+							{this.degdirec2(this.state.dailyWDArr[1])}
+						</td>
+						<td>
+							
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Day 2
+						</td>
+						<td>
+							{this.state.dailyTArr[2]}
+						</td>
+						<td>
+							<img src = {this.state.dailyIArr[2]}> </img>
+						</td>
+						<td>
+							{this.state.dailyWSArr[2]}
+						</td>
+						<td>
+							{this.degdirec2(this.state.dailyWDArr[2])}
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Day 3
+						</td>
+						<td>
+							{this.state.dailyTArr[3]}
+						</td>
+						<td>
+							<img src = {this.state.dailyIArr[3]}/>
+						</td>
+						<td>
+							{this.state.dailyWSArr[3]}
+						</td>
+						<td>
+							{this.degdirec2(this.state.dailyWDArr[3])}
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Day 4
+						</td>
+						<td>
+							{this.state.dailyTArr[4]}
+						</td>
+						<td>
+							<img src = {this.state.dailyIArr[4]}/>		
+						</td>
+						<td>
+							{this.state.dailyWSArr[4]}
+						</td>
+						<td>
+							{this.degdirec2(this.state.dailyWDArr[4])}
+						</td>
+					</tr>
+					<tr>
+						<td>
+							Day 5
+						</td>
+						<td>
+							{this.state.dailyTArr[5]}
+						</td>
+						<td>
+							<img src = {this.state.dailyIArr[5]}/>
+						</td>
+						<td>
+							{this.state.dailyWSArr[5]}
+						</td>
+						<td>
+							{this.degdirec2(this.state.dailyWDArr[5])}
+						</td>
+					</tr>
+				</table>
+			</div>
+		);
+	}
 	// the main render method for the iphone component
 	render() {
 		// check if temperature data is fetched, if so add the sign styling to the page
 		const tempStyles = this.state.temp ? `${style.temperature} ${style.filled}` : style.temperature;
-		
+		const futuretable = this.ShowFuture();
 		// display all weather data
 		return (
 			<div class={ style.container }>
@@ -94,14 +239,19 @@ export default class Iphone extends Component {
 					<div class={ style.conditions }>{ this.state.cond }</div>
 					<span class={ tempStyles }>{ this.state.temp }</span>
 					<div class={ style.city }>{ this.state.speed }</div>
-					<div class={ style.city }>{ this.state.degree }</div>
 					<div class={ style.city }>{this.degdirec(this.state.degree)}</div>
 				</div>
 				<div class={ style.details }></div>
+				<div>
+					{this.state.future? futuretable:null}
+				</div>
 				<div class= { style_iphone.container }> 
 					{ this.state.display ? <Button class={ style_iphone.button } clickFunction={ this.fetchWeatherData }/ > : null }
 				</div>
+				
 			</div>
+			//function below shows actual degree. 
+			//<div class={ style.city }>{ this.state.degree }</div> 
 		);
 	}
 
@@ -121,22 +271,30 @@ export default class Iphone extends Component {
 			degree : winddeg
 		});      
 	}
+
+	//gets future data from another api different to api fetching current data
 	parseResponse2 = (parsed_json) => {
-		var dailytemp = [];
-		var dailyspeed = [];
-		var dailydegree = [];
+		var dailyTemp = [];
+		var dailySpeed = [];
+		var dailyDegree = [];
+		var dailyIcon = [];
+		var dailyIconLink=[];
 		
-		for (let index = 0; index < 7; index++) {
-			dailytemp.push(parsed_json['daily'][i]['temp']['day']);
-			dailyspeed.push(parsed_json['daily'][i]['wind_speed']);
-			dailydegree.push(parsed_json['daily'][i]['wind_degree']);
+		for (let i = 0; i < 7; i++) {
+			dailyTemp.push(parsed_json['daily'][i]['temp']['day']);
+			dailySpeed.push(parsed_json['daily'][i]['wind_speed']);
+		 	dailyDegree.push(parsed_json['daily'][i]['wind_deg']);
+			dailyIcon.push(parsed_json['daily'][i]['weather']['0']['icon']);
+			dailyIconLink.push("http://openweathermap.org/img/wn/"+dailyIcon[i]+"@2x.png");
 		}
 
 		// set states for fields so they could be rendered later on
 		this.setState({
-			dailytarr : dailytemp,
-			dailywsarr : dailyspeed,
-			dailywdarr : dailydegree
+			dailyTArr : dailyTemp,
+			dailyWSArr : dailySpeed,
+			dailyWDArr : dailyDegree,
+			dailyIArr :  dailyIconLink,
+
 		});      
 	}
 }
